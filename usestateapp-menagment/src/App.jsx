4a2cf3222d7109f39ae5f48a63a5e7558b17a3d3
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-11-26 23:17:06 +0100

    first commit

---
+++ usestateapp-menagment/src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+
+function NameList() {
+  const [list, setList] = useState(['Jack', 'Jill', 'John']);
+  const [name, setName] = useState(() => 'Jack');
+  function onAddName() {
+    // list.push(name);
+    setList([...list, name]);
+    setName('');
+  }
+
+  return (
+    <div>
+      <ul>
+        {list.map((name) => (
+          <li style={{ color: 'red' }} key={name}>
+            {name}
+          </li>
+        ))}
+      </ul>
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onAddName}>Add Name</button>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(10);
+  function addOne() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className='App'>
+      <button onClick={addOne}>Count = {count}</button>
+    </div>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Counter />
+      <NameList />
+    </div>
+  );
+}
+export default App;
